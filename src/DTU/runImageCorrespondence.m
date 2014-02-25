ba1363936f1a3ebd4185987615b16a520ce787d8
benbraithwaite <benbraithwaite@gmail.com>
2014-02-25 10:51:22 +0100

    ROC articles

--- src/DTU/runImageCorrespondence.m
+++ src/DTU/runImageCorrespondence.m
@@ -1,23 +1,32 @@
 clc, clear all
 
-method.detector = 'vl';
-method.detectorArgs = {'peakthreshold',10};
-method.descriptor = 'sift';
-method.descriptorArgs = {'colour','gaussian opponent'};
-[mFunc, mName] = parseMethod(method);
-mDir = ['DTU/results/' mName];
-
+imNumKey = 25;
 setNum = 1;
-imNum = [1 12 24];
+imNum = [1 12 24 26 37 49];
 liNum = 1;
 
-matches = imageCorrespondence(setNum,imNum,liNum,mFunc,mDir);
+method(1) = methodStruct('vl',{'peakthreshold',10}, ...
+    'sift',{'colour','gray'},{'rx-'});
+method(2) = methodStruct('vl',{'peakthreshold',10}, ...
+    'sift',{'colour','rgb bin'},{'gx-'});
+method(3) = methodStruct('vl',{'peakthreshold',10}, ...
+    'sift',{'colour','rgb'},{'go-'});
+method(4) = methodStruct('vl',{'peakthreshold',10}, ...
+    'sift',{'colour','opponent'},{'bo-'});
+method(5) = methodStruct('vl',{'peakthreshold',10}, ...
+    'sift',{'colour','gaussian opponent'},{'ko-'});
 
-figure
-plot(imNum,[matches.Area],'bo-')
-% hold on
-% plot(imNum,[matches.RecallRate],'ro-')
-h = title(mName);
-set(h,'interpreter','none')
-legend('AUC')
-axis([0 50 0 1])
+figure('units','normalized','outerposition',[0 0 1 1])
+axis([imNum(1)-1 imNum(end)+1 0 1])
+hold on
+for i = 1:numel(method)
+    m = method(i);
+    [mFunc, mName{i}] = parseMethod(m);
+    mDir = ['DTU/results/' mName{i}];
+    matches(i,:) = imageCorrespondence(setNum,imNum,liNum,mFunc,mDir);
+    after = find(imNum > imNumKey,1);
+    plot(imNum(1:after-1),[matches(i,1:after-1).Area],m.plotParams{:})
+    plot(imNum(after:end),[matches(i,after:end).Area],m.plotParams{:})
+end
+
+legend([mName],'location','southeast','interpreter','none')
---
+++ src/methodStruct.m
@@ -0,0 +1,10 @@
+function m = methodStruct(det, detArgs, des, desArgs, plotParams)
+
+m.detector = det;
+m.detectorArgs = detArgs;
+m.descriptor = des;
+m.descriptorArgs = desArgs;
+m.plotParams = plotParams;
+
+end
+
