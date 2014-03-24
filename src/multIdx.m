e02c6277c4ae0036dc23c53f3925525e721871f5
benbraithwaite <benbraithwaite@gmail.com>
2014-03-24 11:24:06 +0100

    fixed multIdx

--- src/multIdx.m
+++ src/multIdx.m
@@ -1,8 +1,13 @@
 function x = multIdx(x,varargin)
-%MULTIDX Multiple successive indexing.
+%MULTIDX Multiple successive indexing. Allows for cell array inputs.
 
 for i = 1:numel(varargin)
-    x = x(varargin{i});
+    idx = varargin{i};
+    if iscell(idx)
+        x = x(idx{:});
+    else
+        x = x(idx);
+    end
 end
 
 end
