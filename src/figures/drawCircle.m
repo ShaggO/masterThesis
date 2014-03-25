56efc888853d9d13979ab6e8b12b92decd423d16
Malte Stær Nissen <malte.nissen@gmail.com>
2014-03-25 16:57:59 +0100

    Changed to using vector circles

--- src/figures/drawCircle.m
+++ src/figures/drawCircle.m
@@ -4,20 +4,24 @@ if nargin < 4
     color = 'k';
 end
 
-if size(x,1) > size(x,2)
+if size(x,1) < size(x,2)
     x = x';
 end
-if size(y,1) > size(y,2)
+if size(y,1) < size(y,2)
     y = y';
 end
-if size(r,1) > size(r,2)
+if size(r,1) < size(r,2)
     r = r';
 end
+positions = [x-r y-r 2*r 2*r];
+for i = 1:numel(x)
+    rectangle('position',positions(i,:),'curvature',[1 1],'edgecolor',color)
+end
 
-% Step through the angles
-theta = (0:2*pi/360:2*pi)';
-x = repmat(r,[numel(theta) 1]) .* repmat(cos(theta),[1 size(x,2)]) + repmat(x,[numel(theta) 1]);
-y = repmat(r,[numel(theta) 1]) .* repmat(sin(theta),[1 size(x,2)]) + repmat(y,[numel(theta) 1]);
-plot(x,y,['-' color]);
+% Step through the angles manually
+%theta = (0:2*pi/360:2*pi)';
+%x = repmat(r,[numel(theta) 1]) .* repmat(cos(theta),[1 size(x,2)]) + repmat(x,[numel(theta) 1]);
+%y = repmat(r,[numel(theta) 1]) .* repmat(sin(theta),[1 size(x,2)]) + repmat(y,[numel(theta) 1]);
+%plot(x,y,['-' color]);
 
 end
