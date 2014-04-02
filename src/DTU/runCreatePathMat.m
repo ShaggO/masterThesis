f805726184aaff5e2ed42127e7dbba62ce1b5e62
benbraithwaite <benbraithwaite@gmail.com>
2014-04-02 10:22:44 +0200

    Added runCreatePathMat to gitignore

--- src/DTU/runCreatePathMat.m
+++ src/DTU/runCreatePathMat.m
@@ -1,7 +1,7 @@
 clear all;
 dtuDataSet = 'C:\Users\Ben\Documents\GitHub\masterThesis\src\DTU\img1200x1600';
 dtuDataReconstructions = 'C:\Users\Ben\Documents\GitHub\masterThesis\src\DTU\reconstructions';
-dtuResults = 'C:\Users\Ben\Documents\GitHub\masterThesis\src\DTU\results';
+dtuResults = 'C:\Users\Ben\Documents\GitHub\masterThesis\src\DTU\results1200x1600';
 dtuCalibrationFile = [dtuDataSet '\calibrationFile'];
 dtuDataDiffuse = dtuDataSet;
 
