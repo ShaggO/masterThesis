655f3a04fa494795746da868bf32c638b0a1f8f6
benbraithwaite <benbraithwaite@gmail.com>
2014-04-02 10:31:45 +0200

    deleted runCreatePathMat

--- src/DTU/runCreatePathMat.m
+++
@@ -1,8 +0,0 @@
-clear all;
-dtuDataSet = 'C:\Users\Ben\Documents\GitHub\masterThesis\src\DTU\img1200x1600';
-dtuDataReconstructions = 'C:\Users\Ben\Documents\GitHub\masterThesis\src\DTU\reconstructions';
-dtuResults = 'C:\Users\Ben\Documents\GitHub\masterThesis\src\DTU\results1200x1600';
-dtuCalibrationFile = [dtuDataSet '\calibrationFile'];
-dtuDataDiffuse = dtuDataSet;
-
-save('DTU\paths');
