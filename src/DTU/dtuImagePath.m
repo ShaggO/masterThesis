7a3f24bc43ddd1a5fcf81233e49b368cc8c772a5
Malte Stær Nissen <malte.nissen@gmail.com>
2014-04-04 16:59:34 +0200

    Parameter notes and other stuff from the day (including light paths)

--- src/DTU/dtuImagePath.m
+++ src/DTU/dtuImagePath.m
@@ -1,17 +1,25 @@
-function path = dtuImagePath(setNum, imNum, liNum)
+function path = dtuImagePath(setNum, imNum, liNum,liType)
+
+if nargin < 4
+    liType = 'raw';
+end
+assert(strcmp(liType,'raw') || strcmp(liType,'x') || strcmp(liType,'z'),...
+    ['Lighting type not supported: ' liType '. Must be raw, x or z']);
 
 % Load paths for the data
-% Loads the following variables:
-%   dtuSavePath
-%   dtuDataPath
-%   dtuDataSet
 load('paths');
 
-if isnumeric(liNum)
-    path = sprintf('%s/SET%.3d/Img%.3d_%.2d.bmp',dtuDataSet,setNum,imNum,liNum);
-elseif strcmp(liNum,'diffuse')
-    path = sprintf('%s/SET%.3d/Img%.3d_diffuse.bmp',dtuDataDiffuse,setNum,imNum);
-else
-    error('invalid liNum')
+switch liType
+    case 'raw'
+        if isnumeric(liNum)
+            path = sprintf('%s/SET%.3d/Img%.3d_%.2d.bmp',dtuDataSet,setNum,imNum,liNum);
+        elseif strcmp(liNum,'diffuse')
+            path = sprintf('%s/SET%.3d/Img%.3d_diffuse.bmp',dtuDataGenerated,setNum,imNum);
+        else
+            error('invalid linum');
+        end
+    otherwise
+        path = sprintf('%s/SET%.3d/Img%.3d_light-%s_%.2d.bmp',dtuDataGenerated,setNum,imNum,liType,liNum);
 end
 
+end
