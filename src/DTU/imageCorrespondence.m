d3a6e4a2aa783b967ea516ba7e2be299855c6cf3
Malte Stær Nissen <malte.nissen@gmail.com>
2014-04-14 16:28:23 +0200

    Fixed reshaping bug and allow imageCorrespondence to run over methods

--- src/DTU/imageCorrespondence.m
+++ src/DTU/imageCorrespondence.m
@@ -9,49 +9,51 @@ load('paths');
 
 imNumKey = 25;
 liNumKey = 0;
-mDir = [dtuResults '/' mName];
-N = numel(setNum)*numel(imNum)*numel(liNum);
+N = numel(setNum)*numel(imNum)*numel(liNum)*numel(mFunc);
 imSize = size(imread(dtuImagePath(setNum(1),imNum(1),liNum(1))));
 
 n = 0;
 for s = setNum
     for i = imNum
         for l = liNum
-            n = n + 1;
-            disp([timestamp() ' Image ' num2str(n) '/' num2str(N)]);
-            matchPath = [mDir '/matches_' dtuImageName(s,i,l)];
+            for m = 1:numel(mFunc)
+                mDir = [dtuResults '/' mName{m}];
+                n = n + 1;
+                disp([timestamp() ' Image ' num2str(n) '/' num2str(N)]);
+                matchPath = [mDir '/matches_' dtuImageName(s,i,l)];
 
-            loaded = false;
-            if cache
-                [loaded,matchLoad] = loadIfExist(matchPath,'file');
-            end
-            if loaded
-                disp('Matches loaded');
-                match = matchLoad.match;
-            else
-                match.setNum = s;
-                match.imNum = i;
-                match.liNum = l;
-                match.imSize = imSize;
-
-                [match.coord,D1] = dtuFeatures(match.setNum,match.imNum,match.liNum,mFunc);
-                [match.coordKey,D2] = dtuFeatures(match.setNum,imNumKey,liNumKey,mFunc);
-
-                [match.matchIdx, match.dist] = featureMatch(D1,D2);
-                match.distRatio = match.dist(:,1) ./ (match.dist(:,2) + eps);
-                match.area = repmat([0.16 0 0.16],[size(D1,1) 1]);
-                match.areaKey = repmat([0.16 0 0.16],[size(D2,1) 1]);
-
-                match = evalCorrespondence(match);
-
-                % Save to file
-                if ~exist(mDir,'dir')
-                    mkdir(mDir)
+                loaded = false;
+                if cache(m)
+                    [loaded,matchLoad] = loadIfExist(matchPath,'file');
                 end
-                save(matchPath,'match')
-            end
+                if loaded
+                    disp('Matches loaded');
+                    match = matchLoad.match;
+                else
+                    match.setNum = s;
+                    match.imNum = i;
+                    match.liNum = l;
+                    match.imSize = imSize;
 
-            matches(n) = match;
+                    [match.coord,D1] = dtuFeatures(match.setNum,match.imNum,match.liNum,mFunc{m});
+                    [match.coordKey,D2] = dtuFeatures(match.setNum,imNumKey,liNumKey,mFunc{m});
+
+                    [match.matchIdx, match.dist] = featureMatch(D1,D2);
+                    match.distRatio = match.dist(:,1) ./ (match.dist(:,2) + eps);
+                    match.area = repmat([0.16 0 0.16],[size(D1,1) 1]);
+                    match.areaKey = repmat([0.16 0 0.16],[size(D2,1) 1]);
+
+                    match = evalCorrespondence(match);
+
+                    % Save to file
+                    if ~exist(mDir,'dir')
+                        mkdir(mDir)
+                    end
+                    save(matchPath,'match')
+                end
+
+                matches(n) = match;
+            end
         end
     end
 end
