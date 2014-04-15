99422618e47799de78431ff90e1a53982cf0d840
Malte Stær Nissen <malte.nissen@gmail.com>
2014-04-15 16:19:04 +0200

    Fixed load syntax

--- src/DTU/loadDtuImage.m
+++ src/DTU/loadDtuImage.m
@@ -2,7 +2,7 @@ function I = loadDtuImage(setNum,imNum,liNum)
 %LOADDTUIMAGE Load a DTU image and convert it to single precision. Also
 %scales diffuse images to make them brighter.
 
-I = loadDtuImage(setNum,imNum,liNum));
+I = loadDtuImage(setNum,imNum,liNum);
 I = im2single(I);
 if liNum == 0
     I = 1.2*I;
