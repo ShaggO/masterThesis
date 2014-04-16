d5f298398515b2d1ff1ad151f89b042bf18bc69b
Malte Stær Nissen <malte.nissen@gmail.com>
2014-04-16 14:58:13 +0200

    Fixed hashing algorithm down to 5 decimal precision

--- src/imageHash.m
+++ src/imageHash.m
@@ -1,9 +1,8 @@
 function h = imageHash(I)
 %IMAGEHASH Summary of this function goes here
 
-rng(1)
-I = I .* randi(2^20,size(I));
+rng(1);
+I = round(double(I)*10^5) .* randi(10^5,size(I));
 h = sum(I(:));
 
 end
-
