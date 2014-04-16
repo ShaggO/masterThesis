a57108d34816bfb319423aeadac4e5eb2cd52057
Malte Stær Nissen (image server) <malte.nissen@gmail.com>
2014-04-16 17:05:48 +0200

    Added script for moving scalespaces to new hash values

--- src/imageHashOldest.m
+++ src/imageHashOldest.m
@@ -1,8 +1,8 @@
-function h = imageHash(I)
+function h = imageHashOldest(I)
 %IMAGEHASH Summary of this function goes here
 
 rng(1,'combRecursive');
-I = round(double(I)*10^5) .* randi(10^5,size(I));
+I = I .* randi(2^20,size(I));
 h = sum(I(:));
 
 end
