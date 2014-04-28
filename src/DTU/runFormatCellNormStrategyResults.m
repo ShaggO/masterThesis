b250c78625fd07e328d0bd9624f4e7a96112284f
Malte Stær Nissen <malte.nissen@gmail.com>
2014-04-28 15:53:58 +0200

    Added script for formatting specific cell norm strategy results

---
+++ src/DTU/runFormatCellNormStrategyResults.m
@@ -0,0 +1,57 @@
+clear all; clc;
+
+setNum = 1:60;
+mNum = 4;
+testType = 'train';
+pathTypes = 1:6;
+
+[imNumKey,~,imNum,liNum,pathLabels] = dtuPaths(testType);
+
+% Number of images in each set
+imgNum = sum(cellfun(@numel,imNum(pathTypes)) .* ...
+    cellfun(@numel,liNum(pathTypes))) * numel(setNum);
+
+% Generate indexing matrix
+idx2spil = zeros(imgNum,4);
+
+n = 0;
+for s = setNum
+    for p = pathTypes
+        for i = imNum{p}
+            for l = liNum{p}
+                n = n+1;
+                idx2spil(n,:) = [s,p,i,l];
+            end
+        end
+    end
+end
+
+load('paths');
+load([dtuResults '/resultsCellNormStrategy.mat']);
+PRmeans = zeros(numel(pathTypes),numel(method));
+ROCmeans = zeros(numel(pathTypes),numel(method));
+PRstd = PRmeans;
+ROCstd = ROCmeans;
+for i = pathTypes
+    PRmeans(i,:) = mean(PR(idx2spil(:,2) == i,:),1);
+    PRstd(i,:) = std(PR(idx2spil(:,2) == i,:),1,1);
+    ROCmeans(i,:) = mean(ROC(idx2spil(:,2) == i,:),1);
+    ROCstd(i,:) = std(ROC(idx2spil(:,2) == i,:),1,1);
+end
+
+Xs = repmat((1:numel(pathTypes))',[1 numel(method)]);
+
+labels = {'Strat 0','Strat 1','Strat 2','Strat 3'};
+
+figure;
+errorbar(Xs,PRmeans,PRstd);
+hold on;
+legend(labels{:});
+xlabel('pathtype');
+ylabel('mean PR auc');
+
+figure;
+errorbar(Xs,ROCmeans,ROCstd);
+legend(labels{:});
+xlabel('pathtype');
+ylabel('mean ROC auc');
