34efa3c7083086ca15380b0b55fd7aaeb7f4663b
benbraithwaite <benbraithwaite@gmail.com>
2014-05-05 14:06:05 +0200

    Cropped figures

--- src/figures/runScaleSpaceFigures.m
+++ src/figures/runScaleSpaceFigures.m
@@ -4,17 +4,25 @@ load('cellHistExample')
 
 figure
 visualizeScaleSpaces({L.none},scales,[0 1],1,P,validP);
-saveas(gcf,'../report/img/cellHistScaleSpacesP.png')
+path = '../report/img/cellHistScaleSpacesP.png';
+saveas(gcf,path)
+cropPng(path)
 
 figure
 visualizeScaleSpaces(Vscales,scales,[-pi pi],-0.7*pi);
 colormap('hsv')
-saveas(gcf,'../report/img/cellHistScaleSpacesV.png')
+path = '../report/img/cellHistScaleSpacesV.png';
+saveas(gcf,path)
+cropPng(path)
 
 figure
 visualizeScaleSpaces(Mscales,scales,[0 0.4],1);
-saveas(gcf,'../report/img/cellHistScaleSpacesM.png')
+path = '../report/img/cellHistScaleSpacesM.png';
+saveas(gcf,path)
+cropPng(path)
 
 figure
 visualizeScaleSpaces(MscalesNorm,scales,[],max(M));
-saveas(gcf,'../report/img/cellHistScaleSpacesMnorm.png')
+path = '../report/img/cellHistScaleSpacesMnorm.png';
+saveas(gcf,path)
+cropPng(path)
