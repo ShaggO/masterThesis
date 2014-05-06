84d08646946167575a785e6df1912133d2859dfd
benbraithwaite <benbraithwaite@gmail.com>
2014-05-06 14:55:22 +0200

    Better legend

--- src/figures/runGridTypeLegend.m
+++ src/figures/runGridTypeLegend.m
@@ -1,16 +1,16 @@
 clc, clear all
 
-fig('unit','inches','width',15,'height',2,'fontsize',8)
+fig('unit','inches','width',12,'height',1,'fontsize',8)
 hold on
 plot(0,0,'rx')
 plot(0,0,'k-')
 plot(0,0,'g-')
 plot(0,0,'b-')
 
-h=legend(gca,'Cell centers','Cell \sigma','Detection \sigma','Grid radius', ...
+h=legend(gca,'Cell centers','Cell SD','Detection scale','Grid radius', ...
     'Orientation','horizontal');
 set(allchild(gca),'visible','off');
 set(gca,'visible','off')
 set(h,'OuterPosition',[0 0 1 1]);
 
-saveTightFigure(gcf,'../report/img/legendTest.pdf')
+saveTightFigure(gcf,'../report/img/gridType_legend.pdf')
