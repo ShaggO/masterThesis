4f5a5706d1d3c7bc176eed695d6ac03eebe6fb91
benbraithwaite <benbraithwaite@gmail.com>
2014-05-06 14:49:38 +0200

    Legend test

--- src/cellHist/runCreateCellOffsets.m
+++ src/cellHist/runCreateCellOffsets.m
@@ -1,7 +1,7 @@
 clc, clear all
 
-gridType = 'polar';
-gridSize = [6 4];
+gridType = 'concentric polar central';
+gridSize = [8 3];
 gridRadius = 50;
 cellSigma = [50 50];
 % cellSigma = [pi/gridSize(1) gridSpacing/2];
