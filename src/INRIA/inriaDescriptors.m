6c87698ff4bec31e5fc3b5a8889bd0a56be3a4fe
benbraithwaite <benbraithwaite@gmail.com>
2014-05-22 17:41:18 +0200

    Removed profiler test

--- src/INRIA/inriaDescriptors.m
+++ src/INRIA/inriaDescriptors.m
@@ -5,15 +5,11 @@ load('paths')
 for i = 1:numel(images)
     disp(['Image ' num2str(i) '/' num2str(numel(images))]);
     I = im2single(images(i).image);
-    profile on
     [~,Di] = mFunc(I,inriaResults,num2str(i),false);
-    profile off
     if i == 1
         D = zeros(numel(images),numel(Di));
     end
     D(i,:) = reshape(Di',1,[]);
-    return
 end
 
 end
-
