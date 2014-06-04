422e910d04e7136f19b7dc859486bedd89464b4b
Malte Stær Nissen <malte.nissen@gmail.com>
2014-06-04 16:45:00 +0200

    Script to run test on optimal parameters

---
+++ src/DTU/optimizeParams/runOptimalTest.m
@@ -0,0 +1,34 @@
+clear all; clc;
+
+pathTypes = 1:6;
+display = false;
+runInParallel = true;
+
+%% Load Go and Si splits
+splits = 6;
+for i = 1:splits
+    go(i) = load(['results/optimize/parameterStudyGo_' num2str(i) '-of-' num2str(splits) '.mat']);
+    si(i) = load(['results/optimize/parameterStudySi_' num2str(i) '-of-' num2str(splits) '.mat']);
+end
+
+% Generate method and setnum cells
+chosenGo = go(3).method;
+chosenGo = modifyDescriptor(chosenGo,'normSigma',[1.6 1.6]);
+
+chosenSi = si(1).method;
+methods = {[go.method],chosenGo,[si.method],chosenSi};
+setNums = {[go.setNumTest],(1:60)',[si.setNumTest],(1:60)'};
+
+% Compute ROC and PR on test
+ROC  = cell(size(methods));
+PR   = ROC;
+for i = 1:numel(methods)
+    for j = 1:numel(methods{i})
+        [ROCi,PRi] = dtuTest(setNums{i}(:,j),methods{i}(j),pathTypes,display,runInParallel,'test');
+        ROC{i} = [ROC{i};ROCi];
+        PR{i} = [PR{i};PRi];
+    end
+end
+
+% Save results
+save('results/optimize/DTUparamsTest');
