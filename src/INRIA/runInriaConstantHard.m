b7786fd95f7f8fe3112ef58eb42996f77b6dcc16
benbraithwaite <benbraithwaite@gmail.com>
2014-06-13 14:04:25 +0200

    Fixed another bug

--- src/INRIA/runInriaConstantHard.m
+++ src/INRIA/runInriaConstantHard.m
@@ -5,9 +5,10 @@ windowSize = [134 70];
 
 name = {'Go','Si','GoSi','Hog'};
 
+svmPath = cell(numel(name),1);
 for i = 1:numel(name)
-    load(['results/optimize/inriaParameters' name{i}]); % SI settings
-    svmPath{i} = inriaTestSvm(method,svmArgs,true,nHard);
+    params = load(['results/optimize/inriaParameters' name{i}]); % SI settings
+    svmPath{i} = inriaTestSvm(params.method,params.svmArgs,true,nHard);
 end
 
 save('results/inriaConstantHard','svmPath')
