3323dcbe1038402632670fc6884619e525a9cbdb
benbraithwaite <benbraithwaite@gmail.com>
2014-06-17 17:08:30 +0200

    Made bin filter figures slightly prettier

--- src/figures/runDrawBinFilters.m
+++ src/figures/runDrawBinFilters.m
@@ -8,7 +8,7 @@ right = 1;
 binCount = 4;
 binCArgin = {};
 period = 1;
-colour = {'k-','b-','m-','r-'};
+colour = {'c-','r-','g-','b-'};
 options = {'-r300'};
 
 fig('unit','inches','width',6,'height',4,'fontsize',8)
@@ -20,18 +20,12 @@ drawBinFilters('box',binSigma,left,right,binCount,binCArgin,period,colour)
 export_fig('../report/img/binFilterBoxPeriodic.pdf',options{:})
 
 fig('unit','inches','width',6,'height',4,'fontsize',8)
-drawBinFilters('triangle',binSigma,left,right,binCount,binCArgin,period,colour)
+drawBinFilters('triangle',binSigma-0.01,left,right,binCount,binCArgin,period,colour)
 export_fig('../report/img/binFilterTrianglePeriodic.pdf',options{:})
 
 %% Renormalized non-periodic
 
-binSigma = 1/2;
-left = -1;
-right = 1;
-binCount = 4;
-binCArgin = {};
 period = 0;
-colour = {'k-','b-','m-','r-'};
 
 fig('unit','inches','width',6,'height',4,'fontsize',8)
 drawBinFilters('gaussian',binSigma,left,right,binCount,binCArgin,period,colour)
