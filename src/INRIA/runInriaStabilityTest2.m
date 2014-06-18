0e7288460ef615595d857e756c9fce74f94aa806
Malte @ imagecluster <malte.nissen@gmail.com>
2014-06-18 18:56:03 +0200

    Stability splitted into two scripts. Parallel testSvm

---
+++ src/INRIA/runInriaStabilityTest2.m
@@ -0,0 +1,20 @@
+clc, clear all
+
+nHard = 10^5;
+nWindows = [1 2 5 10 20 40 60];
+seed = (6:10)*10^4;
+%nWindows = 40;
+%seed = 2*10^4;
+name = {'Go','Si','GoSi','Hog','HogDT'};
+
+start = tic;
+for i = 1:numel(name)
+    params = load(['results/optimize/inriaParameters' name{i}]); % SI settings
+    for j = 1:numel(nWindows)
+        for k = 1:numel(seed)
+            svmPath = inriaTestSvm(params.method,params.svmArgs,true,nHard,nWindows(j),seed(k));
+            copyfile(svmPath,['results/inriaTestSvm' name{i} '100k_' num2str(nWindows(j)) '_' num2str(seed(k)) '.mat'])
+        end
+    end
+end
+stop = toc(start)
