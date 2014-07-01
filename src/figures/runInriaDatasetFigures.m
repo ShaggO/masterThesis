5eb2cd6f1482c0238c40fa6aeb8bc572b8ef6f92
benbraithwaite <benbraithwaite@gmail.com>
2014-07-01 16:36:28 +0200

    INRIA figures, experimental setup

--- src/figures/runInriaDatasetFigures.m
+++ src/figures/runInriaDatasetFigures.m
@@ -18,48 +18,48 @@ binSigmaS = 2 ./ (2*8);
 binCS = createBinCenters(-1,1,binCountS);
 
 load('paths')
-load([inriaDataSet '/inriaTrain']);
+load([inriaDataSet '/inriaPosTrain']);
 
 Mtotal = zeros(128,64);
 Stotal = Mtotal;
 Ctotal = Mtotal;
 BTtotal = zeros([128 64 binCountS]);
 BStotal = BTtotal;
-for i = 1:numel(posTrain)
-    disp([num2str(i) '/' num2str(numel(posTrain))])
-    I = rgb2gray(im2single(posTrain(i).image));
+for i = 1:numel(images)
+    disp([num2str(i) '/' num2str(numel(images))])
+    I = rgb2gray(im2single(images(i).image));
     L = dGaussScaleSpace(I,d,scales,rescale,smooth);
     L = structfun(@(x) x(4:end-3,4:end-3),L,'UniformOutput',0); % remove image border
     T = diffStructure('Theta',L,1);
     S = diffStructure('S',L,1);
-%     M = diffStructure('M',L,1);
-%     C = diffStructure('C',L,1);
-%     Mtotal = Mtotal + M{1};
-%     Stotal = Stotal + S{1};
-%     Ctotal = Ctotal + C{1};
-    BT = ndBinWeights(T{1}(:),binCT,binFT,binRT,'period',periodT);
-    BTtotal = BTtotal + reshape(BT,[128 64 binCountT]);
-    BS = ndBinWeights(S{1}(:),binCS,binFS,binRS,'period',periodS);
-    BStotal = BStotal + reshape(BS,[128 64 binCountS]);
+    M = diffStructure('M',L,1);
+    C = diffStructure('C',L,1);
+    Mtotal = Mtotal + M{1};
+    Stotal = Stotal + S{1};
+    Ctotal = Ctotal + C{1};
+%     BT = ndBinWeights(T{1}(:),binCT,binFT,binRT,'period',periodT);
+%     BTtotal = BTtotal + reshape(BT,[128 64 binCountT]);
+%     BS = ndBinWeights(S{1}(:),binCS,binFS,binRS,'period',periodS);
+%     BStotal = BStotal + reshape(BS,[128 64 binCountS]);
 end
 
-% Mtotal = Mtotal / max(Mtotal(:));
-% imwrite(Mtotal,'../report/img/inriaPosTrainMeanM.png')
-% 
-% Stotal = (Stotal - min(Stotal(:))) / (max(Stotal(:)) - min(Stotal(:)));
-% imwrite(Stotal,'../report/img/inriaPosTrainMeanS.png')
-% 
-% Ctotal = Ctotal / max(Ctotal(:));
-% imwrite(Ctotal,'../report/img/inriaPosTrainMeanC.png')
+Mtotal = Mtotal / max(Mtotal(:));
+imwrite(Mtotal,'../report/img/inriaPosTrainMeanM.png')
 
-[~,Tmode] = max(BTtotal,[],3);
-figure
-imshow(imresize(binCT(Tmode),5,'box'),[])
-colormap('hsv')
-colorbar
+Stotal = (Stotal - min(Stotal(:))) / (max(Stotal(:)) - min(Stotal(:)));
+imwrite(Stotal,'../report/img/inriaPosTrainMeanS.png')
 
-[~,Smode] = max(BStotal,[],3);
-figure
-imshow(imresize(binCS(Smode),5,'box'),[])
-colormap('jet')
-colorbar
+Ctotal = Ctotal / max(Ctotal(:));
+imwrite(Ctotal,'../report/img/inriaPosTrainMeanC.png')
+
+% [~,Tmode] = max(BTtotal,[],3);
+% figure
+% imshow(imresize(binCT(Tmode),5,'box'),[])
+% colormap('hsv')
+% colorbar
+% 
+% [~,Smode] = max(BStotal,[],3);
+% figure
+% imshow(imresize(binCS(Smode),5,'box'),[])
+% colormap('jet')
+% colorbar
