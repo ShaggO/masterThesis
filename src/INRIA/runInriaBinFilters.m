ac53c868ab37049c727ac88325d4d6a7a7dd8fcf
Malte @ imagecluster <malte.nissen@gmail.com>
2014-07-03 16:46:10 +0200

    Fixed creation of inriaData

--- src/INRIA/runInriaBinFilters.m
+++ src/INRIA/runInriaBinFilters.m
@@ -3,9 +3,10 @@ clc, clear all
 name = {'Go','Si'};
 for i = 1:numel(name)
     load(['results/optimize/inriaParameters' name{i}]);
-    
+
+    data = inriaData;
     loggerBinFilter = handler(struct('parameter',{},'iteration',{},'values',{},'PRAUC',{}));
     [~] = inriaOptimizeEnum(data,diaryFile,loggerBinFilter,method,svmArgs,'binFilter',{'box','triangle','gaussian'});
-    
+    clear data; 
     save(['results/optimize/inriaParameters' name{i}]);
 end
