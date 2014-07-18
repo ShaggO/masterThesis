7d51e9bdc0128678d14a2d1be2011b73947dc09e
Malte Stær Nissen <malte.nissen@gmail.com>
2014-07-18 15:17:32 +0200

    Added small and big chosen to INRIA results graph script

--- src/INRIA/plotInriaResults.m
+++ src/INRIA/plotInriaResults.m
@@ -6,7 +6,7 @@ end
 
 % colours = {'r-','b-','c-','k-','r:','b:','c:','k:'};
 % colours = {'b-','g-','c-','r-','m-','b:','g:','c:','r:','m:'};
-colours = {'r-','g-','b-','k-','r--','r-.'};
+colours = {'r-','g-','b-','k-','r--','r-.','k--'};
 
 ROC = cell(1,numel(svmPath));
 PR = ROC;
