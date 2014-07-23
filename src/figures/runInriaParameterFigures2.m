12a8a5f303d811c82a27dd9e33313f1727c8f376
Malte Stær Nissen <malte.nissen@gmail.com>
2014-07-23 10:01:30 +0200

    Parameter study figures cleanup

--- src/figures/runInriaParameterFigures2.m
+++ src/figures/runInriaParameterFigures2.m
@@ -1,20 +1,34 @@
 clc, clear all, close all
 
-params = load('results/optimize/inriaParametersGo');
-logger = params.loggerParameterResults.data';
+names = {'inriaParametersGo','inriaParametersSi'};
+yRanges = {[0.985 1],[0.935 1]};
+yRangeDims = {[0 12000],[0 11000]};
+yRangeCellSigmaAlt = {[0.992 1],[0.96 1]};
+yRangeBinSigmaAlt = {[0.996 1],[0.978 1]};
 
-logger(1).values(end) = [];
-logger(2).values(end) = [];
+for i = 1:numel(names)
+    if i == 1
+        continue;
+    end
+    name = names{i};
+    yRange = yRanges{i};
+    yRangeDim = yRangeDims{i};
+    params = load(['results/optimize/' name ]);
+    logger = params.loggerParameterResults.data';
 
-plotLoggerResults(logger(3),'\alpha','cellSigma',{},false)
-plotLoggerResults(logger(4),'\beta','binSigma',{},false)
-plotLoggerResults(logger(5),'n','binCount',{},false)
-plotLoggerResults(logger(5),'n','binCount',{},true)
-plotLoggerResults(logger(6),'\eta','normSigma',{},false)
-plotLoggerResults(logger(7),'log(C)','logC',{},false)
+    logger(1).values(end) = [];
+    logger(2).values(end) = [];
 
-plotLoggerResults(logger(2:-1:1),'r','cellSpacing',{'Square','Triangle','location','best'},false)
-plotLoggerResults(logger(2:-1:1),'r','cellSpacing',{'Square','Triangle','location','best'},true)
+    plotLoggerResults(logger(3),'\alpha',[name '_cellSigma'],{},false,yRanges{i})
+    plotLoggerResults(logger(4),'\beta',[name '_binSigma'],{},false,yRanges{i})
+    plotLoggerResults(logger(5),'n',[name '_binCount'],{},false,yRanges{i})
+    plotLoggerResults(logger(5),'n',[name '_binCount'],{},true,yRangeDims{i})
+    plotLoggerResults(logger(6),'\eta',[name '_normSigma'],{},false,yRanges{i})
+    plotLoggerResults(logger(7),'log(C)',[name '_logC'],{},false,yRanges{i})
 
-plotLoggerResults(logger([3 8 9]),'\alpha','cellSigmaAlt',{'Gaussian','Triangle','Box','location','southeast'},false,[0.992 1],6)
-plotLoggerResults(logger([4 10 11]),'\beta','binSigmaAlt',{'Gaussian','Triangle','Box','location','southeast'},false,[0.996 1],6)
+    plotLoggerResults(logger(2:-1:1),'r',[name '_cellSpacing'],{'Square','Triangle','location','best'},false,yRanges{i})
+    plotLoggerResults(logger(2:-1:1),'r',[name '_cellSpacing'],{'Square','Triangle','location','best'},true,yRangeDims{i})
+
+    plotLoggerResults(logger([3 8 9]),'\alpha',[name '_cellSigmaAlt'],{'Gaussian','Triangle','Box','location','southeast'},false,yRangeCellSigmaAlt{i},6)
+    plotLoggerResults(logger([4 10 11]),'\beta',[name '_binSigmaAlt'],{'Gaussian','Triangle','Box','location','southeast'},false,yRangeBinSigmaAlt{i},6)
+end
