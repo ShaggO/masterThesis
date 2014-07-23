bbec4e96cc18c9a56c6887bda6044de0cce88734
Malte @ imagecluster <malte.nissen@gmail.com>
2014-07-23 11:35:19 +0200

    Workaround to avoid pre-loading descriptors if svm files already exist

--- src/INRIA/inriaCrossValidateSvm.m
+++ src/INRIA/inriaCrossValidateSvm.m
@@ -5,11 +5,39 @@ nMethod = numel(method);
 
 PRAUC = zeros(nSplit,nMethod);
 dims = zeros(1,nMethod);
+
+switch class(svmArgs)
+
+    case 'char'
+        svmArgsString = svmArgs;
+    case 'struct'
+        svmArgsString = svmArgs2string(svmArgs);
+    otherwise
+        error('svmArgs must be string or struct.')
+end
+
+load('paths');
+allExist = true;
+for k = 1:nSplit*nMethod
+    [ks,km] = ind2sub([nSplit nMethod],k);
+    [~, mName] = parseMethod(method(km));
+    desDir = [inriaResults '/' mName];
+    svmPath = [desDir '/svm_validate_' num2str(ks) 'of' num2str(nSplit) '_' svmArgsString '_' num2str(data.nWindows) '_' num2str(data.seed) '.mat'];
+    svmVars = {'prob','Ltest','ROC','PR','ROCAUC','PRAUC'};
+    [loaded,svmLoad] = loadIfExist(svmPath,'file');
+    if ~(loaded && all(ismember(svmVars,fieldnames(svmLoad))))
+        allExist = false;
+        break;
+    end
+end
+
 % pre-calculate descriptors
-for km = 1:nMethod
-    [~,D,~] = data.getDescriptors(method(km),desSave,'posTrain','all',true);
-    data.getDescriptors(method(km),desSave,'negTrainCutouts','all',true);
-    dims(km) = size(D,2);
+if ~allExist
+    for km = 1:nMethod
+        [~,D,~] = data.getDescriptors(method(km),desSave,'posTrain','all',true);
+        data.getDescriptors(method(km),desSave,'negTrainCutouts','all',true);
+        dims(km) = size(D,2);
+    end
 end
 
 if runInParallel && all(dims < 12600)
