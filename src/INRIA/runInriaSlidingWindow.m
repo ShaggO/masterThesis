f4c5a5050dc6150dae515bfebb9190bc482ccb1e
Malte @ imagecluster <malte.nissen@gmail.com>
2014-08-05 15:34:39 +0200

    Sliding window using the final test svms

--- src/INRIA/runInriaSlidingWindow.m
+++ src/INRIA/runInriaSlidingWindow.m
@@ -1,31 +1,31 @@
 clc, clear all
 
-params = load('results/optimize/inriaParametersGo.mat');
-test = load('results/inriaTestSvmGoFinal.mat');
+params = load('results/optimize/inriaParametersGoChosenSmall.mat');
+test = load('results/inriaTestSvmGoChosenSmallFinal.mat');
 % params = load('results/optimize/inriaParametersHog.mat');
 % test = load('results/inriaTestSvmHogFinal.mat');
 paths = load('paths');
 
-% params.method.detectorArgs = {'type','square','scales',2.^(0:0.1:2),'spacing',5};
+ params.method.detectorArgs = {'type','square','scales',2.^(0:0.1:2),'spacing',5};
 
 data = inriaData;
-images = data.loadCache('posTest');
-% I.image = imread([paths.inriaDataSet '/Test/pos/crop001573.png']);
-% I.image = imread([paths.inriaDataSet '/test_64x128_H96/pos/person_293f.png']);
+%images = data.loadCache('posTest');
+I.image = imread([paths.inriaDataSet '/Test/pos/crop001573.png']);
+%I.image = imread([paths.inriaDataSet '/test_64x128_H96/pos/person_293f.png']);
 
 [mFunc, mName] = parseMethod(params.method);
 profile off, profile on
 tic
 
-% [X,D] = inriaDescriptors(images(84),mFunc);
+[X,D] = inriaDescriptors(I,mFunc);
 % [L2,D2] = data.getDescriptors(params.method,false,'posTest',84,false);
 
 %% Test on positive test data
-[LposTest,DposTest] = data.getDescriptors(params.method,false,'posTest','all',false);
-DposTest = sparse(double(DposTest));
-[~,~,probPos] = linearpredict(LposTest,DposTest,test.svm);
+%[LposTest,DposTest] = data.getDescriptors(params.method,false,'posTest','all',false);
+%DposTest = sparse(double(DposTest));
+%[~,~,probPos] = linearpredict(LposTest,DposTest,test.svm);
 
-[probPos test.probPos]
+%[probPos test.probPos]
 
 toc
 profile off
@@ -33,10 +33,10 @@ profile off
 
 % s = D * test.svm.w'
 % 
-% [~,~,p] = linearpredict(1,sparse(double(D)),test.svm)
+[~,~,s] = linearpredict(1,sparse(double(D)),test.svm)
 
-% clear D;
-% save('results/inriaSlidingWindowCompactGo')
+ clear D;
+ save('results/inriaSlidingWindowCompactGo')
 
 % t = -1;
 % idx = s > t;
---
+++ src/INRIA/runInriaSlidingWindowHog.m
@@ -0,0 +1,41 @@
+clc, clear all
+
+params = load('results/optimize/inriaParametersHog.mat');
+test = load('results/inriaTestSvmHogFinal.mat');
+paths = load('paths');
+
+params.method.detectorArgs = {'type','square','scales',2.^(0:0.1:2),'spacing',5};
+
+% images = data.loadCache('negTestFull');
+% I = images(1);
+I.image = imread([paths.inriaDataSet '/Test/pos/crop001573.png']);
+
+[mFunc, mName] = parseMethod(params.method);
+profile off, profile on
+tic
+
+data = inriaData;
+
+[X,D] = inriaDescriptors(I,mFunc);
+
+toc
+profile off
+% profile viewer
+
+s = D * test.svm.w';
+clear D;
+
+save('results/inriaSlidingWindowHog');
+
+% t = -1;
+% idx = s > t;
+
+% figure
+% imshow(I.image)
+% hold on
+% for i = find(idx)'
+%     k = min((s(i)-t)/2,1)/2;
+%     col = [0.5+k 0.5+k 0.5-k];
+%     plot(ones(1,5)*X(i,1)+[-35 35 35 -35 -35]*X(i,3), ...
+%         ones(1,5)*X(i,2)+[-67 -67 67 67 -67]*X(i,3),'color',col);
+% end
