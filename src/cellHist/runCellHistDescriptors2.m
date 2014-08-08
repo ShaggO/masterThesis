8c80689960a946d384c41c9464ff3b90f8cf3838
Malte Stær Nissen <malte.nissen@gmail.com>
2014-08-08 13:52:18 +0200

    New inria example figures

--- src/cellHist/runCellHistDescriptors2.m
+++ src/cellHist/runCellHistDescriptors2.m
@@ -4,7 +4,7 @@ load('results/optimize/inriaParametersGo.mat')
 % method = modifyDescriptor(method,'gridSize',4,'cellFilter','triangle');
 % method = modifyDescriptor(method,'cellSigma',[10 10]);
 % method = modifyDescriptor(method,'colour','none');
-method = modifyDescriptor(method,'cellFilter','box','cellSigma',[3.3 3.3],'binFilter','triangle','binSigma',1);
+%method = modifyDescriptor(method,'cellFilter','box','cellSigma',[3.3 3.3],'binFilter','triangle','binSigma',1);
 
 [mFunc, mName] = parseMethod(method);
 profile off, profile on
@@ -12,7 +12,7 @@ tic
 
 data = inriaData;
 images = data.loadCache('posTest');
-[X,D] = inriaDescriptors(images(1),mFunc);
+[X,D] = inriaDescriptors(images(330),mFunc);
 
 toc
 profile off
