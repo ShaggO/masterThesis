430e97d20b9f943c7284577c28b8125d334f7285
Malte @ imagecluster <malte.nissen@gmail.com>
2014-08-18 15:24:16 +0200

    Minor scale space generation change

--- src/INRIA/runGenerateInriaScaleSpaces.m
+++ src/INRIA/runGenerateInriaScaleSpaces.m
@@ -7,7 +7,7 @@ data.loadCache('all');
 scaleBase = 2^(1/3);
 scaleOffset = 0;
 sigmaRange = [1 16];
-colour = 'gray';
+colour = 'none';
 d = kJetCoeffs(2);
 rescale = 1;
 smooth = false;
@@ -17,7 +17,7 @@ pixelDiff = 1;
 scales = approxScales(sigmaRange,scaleBase,scaleOffset);
 
 load('paths')
-images = [data.posTrain; data.negTrainCutouts; data.negTrainFull; data.posTest; data.negTestCutouts; data.negTestFull];
+images = [data.posTrain; data.negTrainFull; data.posTest; data.negTestFull];
 %images = [data.negTrainFull; data.negTestFull];
 
 sDir = [dtuResults '/scaleSpaces'];
@@ -33,24 +33,27 @@ parfor i = 1:numel(images)
     if mod(i,100) == 0
         disp([timestamp(start) ' Image ' num2str(i) '/' num2str(numel(images))]);
     end
-    I = colourTransform(im2single(img.image),colour);
-    hash = num2str(imageHash(I(:)));
+    I = im2single(img.image);
+    for j = 1:size(I,3)
+        Ij = I(:,:,j);
+    	hash = num2str(imageHash(Ij(:)));
 
-    sPath = [sDir '/' hash '.mat'];
-    [S,Isizes,file] = dGaussScaleSpace(I,d,scales,rescale,smooth);
-    parSave(sPath,'S',S,'Isizes',Isizes,'scales',scales,...
-        'scaleBase',scaleBase,'scaleOffset',scaleOffset,...
-        'sigmaRange',sigmaRange,'colour',colour,'d',d,...
-        'rescale',rescale,'chain',chain,'pixelDiff',pixelDiff,...
-        'smooth',smooth,'imgPath',img.path);
+    	sPath = [sDir '/' hash '.mat'];
+    	[S,Isizes,file] = dGaussScaleSpace(Ij,d,scales,rescale,smooth);
+    	parSave(sPath,'S',S,'Isizes',Isizes,'scales',scales,...
+    	    'scaleBase',scaleBase,'scaleOffset',scaleOffset,...
+    	    'sigmaRange',sigmaRange,'colour',colour,'d',d,...
+    	    'rescale',rescale,'chain',chain,'pixelDiff',pixelDiff,...
+    	    'smooth',smooth,'imgPath',img.path);
+    end
 end
 after = numel(dir(sDir));
 
 disp(['Number of images: ' num2str(numel(images))]);
 disp(['Number of files created: ' num2str(after - before)]);
 
-figure, imshow(S(3).xx);
-figure, imshow(file.S(3).xx)
-figure, imshow(S(3).yy);
-figure, imshow(file.S(3).yy)
+%figure, imshow(S(3).xx);
+%figure, imshow(file.S(3).xx)
+%figure, imshow(S(3).yy);
+%figure, imshow(file.S(3).yy)
 toc(start)
