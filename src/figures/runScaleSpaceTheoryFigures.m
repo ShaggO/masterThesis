bdff7d0c03fb196b7b6aa5f6e5be3a84b9c2c7e9
Malte Stær Nissen <malte.nissen@gmail.com>
2014-08-26 15:31:49 +0200

    Scale space images

--- src/figures/runScaleSpaceTheoryFigures.m
+++ src/figures/runScaleSpaceTheoryFigures.m
@@ -1,18 +1,19 @@
 clc, clear
 
-I = imread('..\data\inrIAPerson\Train\neg\no_person__no_bike_183.png');
+%I = imread('..\data\inrIAPerson\Train\neg\no_person__no_bike_183.png');
+I = imread('../defence/img/shoeOriginal.jpg');
 I = rgb2gray(im2double(I));
 
 scales = 2.^(0:5);
 S = dGaussScaleSpace(I,[0 0],scales,0,1);
 
-imwrite(I,'../report/img/scaleSpaceTheory_0.png');
+imwrite(I,'../defence/img/scaleSpaceTheory_0.png');
 for i = 1:numel(scales)
-    imwrite(S(i).none,['../report/img/scaleSpaceTheory_' num2str(scales(i)) '.png']);
+    imwrite(S(i).none,['../defence/img/scaleSpaceTheory_' num2str(scales(i)) '.png']);
 end
 
 for i = [1 4]
     DoG = S(i+1).none - S(i).none;
     DoG = (DoG + 0.2) / 0.4;
-    imwrite(DoG,['../report/img/scaleSpaceTheory_' num2str(scales(i+1)) '-' num2str(scales(i)) '.png']);
+    imwrite(DoG,['../defence/img/scaleSpaceTheory_' num2str(scales(i+1)) '-' num2str(scales(i)) '.png']);
 end
